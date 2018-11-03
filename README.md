9e8570dc800205da1a497bb78b4c9930fd8cbf4a
Tran Anh Dung <trandunguet@gmail.com>
2018-11-03 18:22:06 +0700

    Show camera

---
+++ README.md
@@ -0,0 +1,12 @@
+# Openni2, OpenCV
+$ sudo apt install libopenni2-dev libopencv-dev
+
+# Kinect driver
+https://github.com/OpenKinect/libfreenect2
+
+# Build & run
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ ./Record
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
-#include <OpenNI.h>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include "OpenNIHelper.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "ok" << std::endl;
+    cv::Mat depth, color;
+    ni::openni2_init();
+    while (true)
+    {
+        ni::openni2_getmat(color, depth);
+
+        cv::imshow("color", color);
+        cv::Mat adjMap;
+        convertScaleAbs(depth, adjMap, 255.0 / 6000);
+        cv::imshow("depth", adjMap);
+        if (cv::waitKey(1) == 27) break;
+    }
+    ni::openni2_destroy();
+    return 0;
 }
